4dcf3901a1cbd7bc0df76ddb5db6380bde82e0cd
vhs <0xc000007b@tutanota.com>
2017-05-24 16:24:16 +0800

    build(Kahlan): output coverage report to file system
    enables coverage reporting of unit and integration tests to codecov

--- kahlan-config.php
+++ kahlan-config.php
@@ -5,26 +5,26 @@ use Kahlan\Reporter\Coverage\Exporter\Clover;
 // Override default options.
 // https://kahlan.github.io/docs/cli-options.html
 $commandLine = $this->commandLine();
-
-// Export code coverage report into a Clover XML format.
-// Use the same directory and filename WordPress Core uses.
-$commandLine->option('coverage-clover', 'default', 'clover.xml');
+$commandLine->option('ff', 'default', 1); // fast fail
+$commandLine->option('coverage', 'default', 2); // organize report by
+$commandLine->option(
+  'coverage-clover', 'default', 'clover.xml'
+); // output clover file to
 
 // Logic to include into the workflow.
 Filter::register('hyperdrive.coverage', function ($chain) {
 
   // Get the reporter called `'coverage'` from the list of reporters.
-  $coverage = $this->reporters()->get('coverage');
-  print_r($coverage);
+  $reporter = $this->reporters()->get('coverage');
 
   // Abort if no coverage is available.
-  if (!$coverage || !$this->commandLine()->exists('coverage-clover')) {
+  if (!$reporter || !$this->commandLine()->exists('coverage-clover')) {
     return $chain->next();
   }
 
   // Use the `Clover` class to write the XML coverage into a file.
   Clover::write([
-    'coverage' => $coverage,
+    'collector' => $reporter,
     'file' => $this->commandLine()->get('coverage-clover'),
     'service_name' => 'travis-ci',
     'service_job_id' => getenv('TRAVIS_JOB_ID') ?: null
