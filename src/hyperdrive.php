95a7f0c5caf114d52c00a0b24a07e2cc823f3ed4
vhs <0xc000007b@tutanota.com>
2017-05-29 16:58:15 +0800

    feat($hyperdrive): upgrade to fetch-inject@1.7.0
    https://www.softwarefreedom.org/resources/2007/gpl-non-gpl-collaboration.html

--- src/hyperdrive.php
+++ src/hyperdrive.php
@@ -18,7 +18,7 @@
  * License URI:     https://codeberg.org/vhs/hyperdrive/blob/master/COPYING
  *
  * Hyperdrive - The fastest way to load pages in WordPress.
- * Copyright (C) 2017  VHS
+ * Copyright (C) 2017  VHS <josh@vhs.codeberg.page>
  *
  * Hyperdrive is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
@@ -33,6 +33,23 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with Hyperdrive.  If not, see
  * https://codeberg.org/vhs/hyperdrive/blob/master/COPYING.
+ *
+ * This file incorporates work covered by the following copyright and
+ * permission notice:
+ *
+ *     Copyright (c) 2017, VHS
+ *
+ *     Permission to use, copy, modify, and/or distribute this software for any
+ *     purpose with or without fee is hereby granted, provided that the above
+ *     copyright notice and this permission notice appear in all copies.
+ *
+ *     THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *     WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *     MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *     ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *     WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *     ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *     OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
 namespace hyperdrive;
@@ -198,21 +215,10 @@ function fold_spacetime( $antimatter_particles ) {
 	}
 
 	return <<<EOD
-/*!
- * Hyperdrive v1.0.0-beta.4
- * Copyright (c) 2017 VHS
- * @license AGPL-3.0 or any later version
- */
-(function () {
-  if (!window.fetch) return;
-  /*!
-   * Fetch Inject v1.6.11
-   * Copyright (c) 2017 VHS
-   * @licence ISC
-   */
-  var fetchInject=function(){"use strict";const e=function(e,t,n,r,o,i,c){i=t.createElement(n),c=t.getElementsByTagName(n)[0],i.appendChild(t.createTextNode(r.text)),i.onload=o(r),c?c.parentNode.insertBefore(i,c):t.head.appendChild(i)},t=function(t,n){if(!t||!Array.isArray(t))return Promise.reject(new Error("`inputs` must be an array"));if(n&&!(n instanceof Promise))return Promise.reject(new Error("`promise` must be a promise"));const r=[],o=n?[].concat(n):[],i=[];return t.forEach(e=>o.push(window.fetch(e).then(e=>{return[e.clone().text(),e.blob()]}).then(e=>{return Promise.all(e).then(e=>{r.push({text:e[0],blob:e[1]})})}))),Promise.all(o).then(()=>{return r.forEach(t=>{i.push({then:n=>{"text/css"===t.blob.type?e(window,document,"style",t,n):e(window,document,"script",t,n)}})}),Promise.all(i)})};return t}();
-  $fetch_inject_string;
-})();
+/*! Hyperdrive v1.0.0-beta.4 | (c) 2017 VHS | @license AGPL-3.0 or any later version */
+/*! Fetch Inject v1.7.0 | (c) 2017 VHS | @license ISC */
+!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?module.exports=t():"function"==typeof define&&define.amd?define(t):e.fetchInject=t()}(this,function(){"use strict";const e=function(e,t,n,o,r,i,c){i=t.createElement(n),c=t.getElementsByTagName(n)[0],i.appendChild(t.createTextNode(o.text)),i.onload=r(o),c?c.parentNode.insertBefore(i,c):t.head.appendChild(i)};return function(t,n){if(!t||!Array.isArray(t))return Promise.reject(new Error("`inputs` must be an array"));if(n&&!(n instanceof Promise))return Promise.reject(new Error("`promise` must be a promise"));const o=[],r=n?[].concat(n):[],i=[];return t.forEach(e=>r.push(window.fetch(e).then(e=>[e.clone().text(),e.blob()]).then(e=>Promise.all(e).then(e=>{o.push({text:e[0],blob:e[1]})})))),Promise.all(r).then(()=>{o.forEach(t=>{i.push({then:n=>{t.blob.type.includes("text/css")?e(window,document,"style",t,n):e(window,document,"script",t,n)}})});return Promise.all(i)})}});
+$fetch_inject_string;
 EOD;
 }
 
